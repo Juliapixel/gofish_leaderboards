afc8bb891bcc0b781bd5d638c59dde95b1b2bc8a
Juliapixel <89038897+Juliapixel@users.noreply.github.com>
2024-03-11 00:31:48 -0300

    make things look less stupid on mobile

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,47 +2,40 @@
     import AsyncMarkdownReq from "$lib/AsyncMarkdownReq.svelte";
 </script>
 
-<h1 class="ml-32 mb-4 mt-8">gofish leaderboards! 🎣 🤩 🏆</h1>
+<div class="ml-8 mr-8 lg:ml-32 lg:mr-32">
+    <h1 class="mb-4 mt-8">gofish leaderboards! 🎣 🤩 🏆</h1>
 
-<div class="ml-32 mr-32">
-    <div class="flex flex-row">
-        <div class="w-1/2">
+    <div class="grid grid-cols-1 lg:grid-cols-2 gap-4">
+        <div>
             <AsyncMarkdownReq
                 url="https://raw.githubusercontent.com/blableblup/gofish/main/leaderboardfish.md"
             />
         </div>
-        <div class="w-1/2">
+        <div>
             <AsyncMarkdownReq
                 url="https://raw.githubusercontent.com/blableblup/gofish/main/leaderboardtotalcount.md"
             />
         </div>
-    </div>
-    <hr />
-    <div class="flex flex-row">
-        <div class="w-1/2">
+        <div>
             <AsyncMarkdownReq
                 url="https://raw.githubusercontent.com/blableblup/gofish/main/leaderboardtrophies.md"
             />
         </div>
-        <div class="w-1/2">
+        <div>
             <AsyncMarkdownReq
                 url="https://raw.githubusercontent.com/blableblup/gofish/main/leaderboardtype.md"
             />
         </div>
-    </div>
-    <hr />
-    <div class="flex flex-row shrink">
-        <div class="w-1/2">
+        <div>
             <AsyncMarkdownReq
                 url="https://raw.githubusercontent.com/blableblup/gofish/main/leaderboardweight.md"
             />
         </div>
     </div>
+    <footer class="mt-12 mb-8">
+        <p>gofish by <a href="https://twitch.tv/breadworms">breadworms</a></p>
+        <p>website source code <a href="https://github.com/Juliapixel/gofish_leaderboards">here</a></p>
+        <p>data sourced from <a href="https://github.com/blableblup/gofish">here</a></p>
+        <p class="text-center mt-8 text-neutral-500">{">~<"}</p>
+    </footer>
 </div>
-<footer class="mt-12 mb-8 ml-32">
-    gofish by <a href="https://twitch.tv/breadworms">breadworms</a>
-    <br />
-    website source code <a href="https://github.com/Juliapixel/gofish_leaderboards">here</a>
-    <br />
-    data sourced from <a href="https://github.com/blableblup/gofish">here</a>
-</footer>
