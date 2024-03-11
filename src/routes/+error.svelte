db2465cd3ec48ce8febe4ccfde0e81945c978054
Juliapixel <89038897+Juliapixel@users.noreply.github.com>
2024-03-10 23:46:09 -0300

    add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import { page } from "$app/stores";
+</script>
+
+<div class="flex flex-col w-full h-[100vh] items-center justify-center">
+    <h1>{$page.status}: {$page.error?.message}</h1>
+    <a href="/" class="mt-8">Go back</a>
+</div>
