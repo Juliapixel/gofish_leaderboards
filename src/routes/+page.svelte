1cd5686f952c888b844c15706354603fb78e6944
Juliapixel <89038897+Juliapixel@users.noreply.github.com>
2024-03-26 20:05:00 -0300

    add more channels

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,20 @@
 <script lang="ts">
     import Leaderboards from "$lib/Leaderboards.svelte";
     import Meta from "$lib/Meta.svelte";
+
+    let selected = "bw";
 </script>
 
 <Meta description="Leaderboards for gofish" title="gofish leaderboards" image="/favicon.png" />
 
 <div class="ml-8 mr-8 lg:ml-32 lg:mr-32">
     <h1 class="mb-4 mt-8">gofish leaderboards! 🎣 🤩 🏆</h1>
+    <select bind:value={selected} class="mt-5 mb-5">
+        <option value="bw">breadworms</option>
+        <option value="psp">psp1g</option>
+    </select>
 
-    <Leaderboards folder="bw" />
+    <Leaderboards folder={selected} />
     <footer class="mt-12 mb-8">
         <p>
             gofish by <a href="https://twitch.tv/breadworms">breadworms</a>
