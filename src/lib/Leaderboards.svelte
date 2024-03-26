046a0aa0a7dc6bea2b8b429265eb8e6e815cd9d0
Juliapixel <89038897+Juliapixel@users.noreply.github.com>
2024-03-26 19:31:34 -0300

    fix leadboard URLs

---
+++ src/lib/Leaderboards.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    import AsyncMarkdownReq from "./AsyncMarkdownReq.svelte";
+
+    export let folder: string;
+</script>
+
+<div class="grid grid-cols-1 lg:grid-cols-2 gap-4">
+    <div>
+        <AsyncMarkdownReq
+            url="https://raw.githubusercontent.com/blableblup/gofish/main/leaderboards/{folder}/leaderboardfish.md"
+        />
+    </div>
+    <div>
+        <AsyncMarkdownReq
+            url="https://raw.githubusercontent.com/blableblup/gofish/main/leaderboards/{folder}/leaderboardtotalcount.md"
+        />
+    </div>
+    <div>
+        <AsyncMarkdownReq
+            url="https://raw.githubusercontent.com/blableblup/gofish/main/leaderboards/{folder}/leaderboardtrophies.md"
+        />
+    </div>
+    <div>
+        <AsyncMarkdownReq
+            url="https://raw.githubusercontent.com/blableblup/gofish/main/leaderboards/{folder}/leaderboardtype.md"
+        />
+    </div>
+    <div>
+        <AsyncMarkdownReq
+            url="https://raw.githubusercontent.com/blableblup/gofish/main/leaderboards/{folder}/leaderboardweight.md"
+        />
+    </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import AsyncMarkdownReq from "$lib/AsyncMarkdownReq.svelte";
+    import Leaderboards from "$lib/Leaderboards.svelte";
     import Meta from "$lib/Meta.svelte";
 </script>
 
@@ -8,33 +8,7 @@
 <div class="ml-8 mr-8 lg:ml-32 lg:mr-32">
     <h1 class="mb-4 mt-8">gofish leaderboards! 🎣 🤩 🏆</h1>
 
-    <div class="grid grid-cols-1 lg:grid-cols-2 gap-4">
-        <div>
-            <AsyncMarkdownReq
-                url="https://raw.githubusercontent.com/blableblup/gofish/main/leaderboardfish.md"
-            />
-        </div>
-        <div>
-            <AsyncMarkdownReq
-                url="https://raw.githubusercontent.com/blableblup/gofish/main/leaderboardtotalcount.md"
-            />
-        </div>
-        <div>
-            <AsyncMarkdownReq
-                url="https://raw.githubusercontent.com/blableblup/gofish/main/leaderboardtrophies.md"
-            />
-        </div>
-        <div>
-            <AsyncMarkdownReq
-                url="https://raw.githubusercontent.com/blableblup/gofish/main/leaderboardtype.md"
-            />
-        </div>
-        <div>
-            <AsyncMarkdownReq
-                url="https://raw.githubusercontent.com/blableblup/gofish/main/leaderboardweight.md"
-            />
-        </div>
-    </div>
+    <Leaderboards folder="bw" />
     <footer class="mt-12 mb-8">
         <p>
             gofish by <a href="https://twitch.tv/breadworms">breadworms</a>
