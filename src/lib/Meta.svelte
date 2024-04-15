6161e6d5005a1f7e46c15623852e043bfda2a04d
Juliapixel <89038897+Juliapixel@users.noreply.github.com>
2024-04-15 00:59:44 -0300

    document Meta.svelte

--- src/lib/Meta.svelte
+++ src/lib/Meta.svelte
@@ -1,8 +1,20 @@
 <script lang="ts">
+    /** description shown on embedded cards */
     export let description: string;
+    /** applies both `<title>` and `<meta name="twitter:title">` */
     export let title: string;
-    export let image: string | undefined = undefined;
+    /**
+     * applies both `<meta name="og:image">` and `<meta name="twitter:image">`
+     *
+     * must be a URL to static image, preferably a .png or .jpeg
+     */
+    export let image: string | URL | undefined = undefined;
 </script>
+
+<!--
+@component
+Modifies the `<head>` of the document to add embed card info to the current page
+-->
 
 <svelte:head>
     <meta name="description" content={description} />
@@ -12,8 +24,8 @@
     <title>{title}</title>
     {#if image}
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:image" content={image} />
-        <meta property="og:image" content={image} />
+        <meta name="twitter:image" content={String(image)} />
+        <meta property="og:image" content={String(image)} />
     {:else}
         <meta name="twitter:card" content="summary" />
     {/if}
