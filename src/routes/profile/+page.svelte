53d67d10ed83faa728c41af9ce2c867972c837f7
Juliapixel <git@juliapixel.com>
2025-07-02 02:19:01 -0300

    add user searching to /profile

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -4,12 +4,43 @@
     import UserProfile from "$lib/UserProfile.svelte";
 
     const urlParams = browser ? new URLSearchParams(location.search) : new URLSearchParams();
-    const user = urlParams.get("user");
+    let user = urlParams.get("user")?.toLowerCase();
+    let searchedUser = user ?? "";
 
     async function getUserID(user: string): Promise<number | undefined> {
         return (await USER_MAP).get(user);
     }
+
+    function setUser(value: string) {
+        user = value.toLowerCase();
+        let url = new URL(location.href);
+        let search = new URLSearchParams(location.search);
+        search.set("user", value.toLowerCase());
+        url.search = search.toString();
+        window.history.replaceState(null, "", url);
+    }
+
+    let timeout: number | undefined;
+    function debounce(fn: () => any): void {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => {
+            fn();
+        }, 500);
+    }
 </script>
+
+<label for="user_search" class="text-lg">Search user:</label>
+<input
+    id="user_search"
+    class="bg-neutral-300 text-black p-0.5 rounded my-6"
+    type="text"
+    bind:value={searchedUser}
+    on:keydown={() => {
+        debounce(() => {
+            setUser(searchedUser);
+        });
+    }}
+/>
 
 {#if user}
     {#await getUserID(user)}
@@ -18,9 +49,10 @@
         {#if userId}
             <UserProfile id={userId} />
         {:else}
+            <h2 class="text-red-400">No user with that name</h2>
             <a href="/">Go back</a>
         {/if}
     {/await}
 {:else}
-    <a href="/">Go back</a>
+    <h1 class="text-neutral-800 text-center mt-12">Nothing...</h1>
 {/if}
